be6ccdadf6484f24aaeea7faf9d266a19b05be7f
DIEGO ALEJANDRO ROJAS REINA <diegoarojas@hotmail.es>
2023-07-28 04:50:21 -0500

    agregando link

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import http from "http";
 import cors from "cors";
 import "dotenv/config";
 import { Server as SocketServer } from "socket.io";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 // import { saveData } from "./db/mongo.js";
 // import Sala from "./models/Sala.js";
 
@@ -11,6 +11,8 @@ const PORT = process.env.PORT_SERVER;
 
 const app = express();
 const secretKey = process.env.SECRET_KEY;
+
+const rooms = [1, 2, 3];
 
 app.use(cors());
 app.use(express.json());
@@ -21,20 +23,25 @@ const io = new SocketServer(server, {
   },
 });
 io.on("connection", (socket) => {
-  socket.on("send-message",(obj)=>{
-    io.emit("chat_message",obj)
-  })
-  
-    // socket.emit("message",()=>{
+  socket.on("JoinRoom", (room) => {
+    socket.join(room);
 
-    // })
+    io.in(room).emit("newUser", "new player has joine the "+ room)
+  });
+  socket.on("chatMessage", ({ room, message }) => {
+    io.to(room).emit("message", message);
+  });
+
+  // socket.emit("message",()=>{
+
+  // })
   // socket.on("joinRoom",({userName, sala})=>{
   //   console.log(userName,sala)
   //   socket.join(sala)
   //   socket.on("message", (body) => {
   //     io.to(sala).emit("message", {
   //       body,
-        
+
   //     });
   //   });
   //   socket.broadcast.to(`${sala}`).emit("message",`${userName} se ha unido al chat`)
@@ -43,9 +50,16 @@ io.on("connection", (socket) => {
 
 app.post("/", async (req, res) => {
   try {
-    
-    const token = jwt.sign({userName: req.body.user, sala:req.body.sala},secretKey,{expiresIn: '1h'});
-    res.json({ status: "ok", redirectUrl: `${process.env.PAGE_URL}/componentes`, token });
+    const token = jwt.sign(
+      { userName: req.body.user, sala: req.body.sala },
+      secretKey,
+      { expiresIn: "1h" }
+    );
+    res.json({
+      status: "ok",
+      redirectUrl: `${process.env.PAGE_URL}/componentes`,
+      token,
+    });
   } catch (err) {
     res.json({ error: err });
   }
